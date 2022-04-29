ac83374f9960af7195ae70bb20c9c187cbe1dad0
elytren <elysetrendell@gmail.com>
2022-04-28 19:12:44 -0700

    completed basic html, css, and psuedocode for js

---
+++ script.js
@@ -0,0 +1,10 @@
+//roll function: when clicked it "rolls" the dice on the screen populating random images
+//function to select certain dice for re-roll
+//buttons that come up and ask if they want to re roll or end turn
+// - should only be able to re roll twice after original roll 
+//should only be allowed to have 13 turns before game ends 
+//numbers inputed into upper section should auto populate a total for upper total score, auto puts number in lower upper section
+//function to check for bonus score
+//lower section auto populate total score
+//grand total score calculates lower and upper combined 
+//show ending score on page
