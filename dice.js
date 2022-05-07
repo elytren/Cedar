d0ba15b745b456d103a627e5df109a761104f053
elytren <elysetrendell@gmail.com>
2022-05-06 17:18:01 -0700

    Completed project

--- dice.js
+++ dice.js
@@ -1,13 +1,11 @@
 let diceface = [
-    "/images/compass.jpeg",
-    "/images/flashlight.jpeg",
-    "/images/hatchet.png",
-    "/images/flask.png", 
-    "/images/tent.png",
-    "/images/s'more.png"
+    "/images/diceface/1.png",
+    "/images/diceface/2.png",
+    "/images/diceface/3.png",
+    "/images/diceface/4.png", 
+    "/images/diceface/5.png",
+    "/images/diceface/6.png"
 ];
-
-const rollbutton = document.getElementById("roll")
 
 let one = document.getElementById('die1');
 let two = document.getElementById('die2');
@@ -18,6 +16,7 @@ let five = document.getElementById('die5');
 let timesRolled = 0;
 let turnNum = 0;
 
+let rollbutton = document.getElementById("roll");
 let reRollBut = document.createElement("button");
     reRollBut.id = "reRoll";
     reRollBut.innerText = "Re-roll";
@@ -26,11 +25,9 @@ let endTurnBut = document.createElement("button");
     endTurnBut.id = "endTurnBut";
     endTurnBut.innerText = "End Turn";
 
-// const diceSel = {one, two, three, four, five}
-
 function getRandomRoll(){
     return diceface[Math.floor(Math.random()* diceface.length)]
-}
+};
 
 rollbutton.addEventListener('click', (e)=> {
     one.src = getRandomRoll();
@@ -43,55 +40,40 @@ rollbutton.addEventListener('click', (e)=> {
     rollbutton.replaceWith((reRollBut), (endTurnBut));
 });
 
-
-
 reRollBut.addEventListener('click', (e)=>{
     timesRolled += 1;
-    console.log(timesRolled)
-    
-    let reRollChoice = prompt("Which die do you want to re-roll");
-    
+    let reRollChoice = prompt("Which die do you want to re-roll", "one, two etc");
     reRollChoice.split(",").forEach(function(dice) {
         let trimmedDice = dice.trim();
-      
         if (trimmedDice == "one") { 
-          one.src = getRandomRoll()
+            one.src = getRandomRoll()
         }
-      
         if (trimmedDice == "two") {
-          two.src = getRandomRoll()
+            two.src = getRandomRoll()
         }
-      
         if (trimmedDice == "three") {
-          three.src = getRandomRoll()
+            three.src = getRandomRoll()
         }
-      
         if (trimmedDice == "four") {
-          four.src = getRandomRoll()
+            four.src = getRandomRoll()
         }
         if (trimmedDice == "five"){
             five.src = getRandomRoll()
-        }
-      });
+        };
+    });
     if (timesRolled >= 3){
         alert("Out of re-rolls, must end turn");
-        window.stop();
-    }
-   
+        reRollChoice.stop();
+    };
 });
-
 
 endTurnBut.addEventListener('click', (e)=>{
     turnNum += 1;
     timesRolled = 0;
     if (turnNum == 13){
-        alert("Stop packing you're ready to go! Score " + Number(grandTot.innerHTML))
+        alert("Stop packing you're ready to go! Score " 
+            + Number(grandTot.innerHTML))
     };
     endTurnBut.remove();
     reRollBut.replaceWith(rollbutton);
-    
-   
-    console.log(timesRolled)
-    console.log(turnNum)
-
 });
--- scorecard.js
+++ scorecard.js
@@ -1,7 +1,3 @@
-//numbers inputed into upper section should auto populate a total for upper total score, auto puts number in lower upper section
-//lower section auto populate total score
-//grand total score calculates lower and upper combined 
-
 let compassInput = document.getElementById('compasses');
 let flashlightInput = document.getElementById('flashlights');
 let hatchetInput = document.getElementById('hatchets');
@@ -9,22 +5,24 @@ let flasksInput = document.getElementById('flasks');
 let tentsInput = document.getElementById('tents');
 let smoresInput = document.getElementById('smores');
 
-let totalScore1 = document.getElementById('totScor')
-let bonusPoints = document.getElementById('bonScor')
-let upperSecTot = document.getElementsByClassName('upTot')
+let totalScore1 = document.getElementById('totScor');
+let bonusPoints = document.getElementById('bonScor');
+let upperSecTot = document.getElementsByClassName('upTot');
 
-let threeKindInput = document.getElementById('3kind')
-let fourKindInput = document.getElementById('4kind')
-let fullPackInput = document.getElementById('fullP')
-let smInput = document.getElementById('smP')
-let lgInput = document.getElementById('lgP')
-let cedarInput = document.getElementById('cedar')
-let lmpInput = document.getElementById('lastMinP')
+let threeKindInput = document.getElementById('3kind');
+let fourKindInput = document.getElementById('4kind');
+let fullPackInput = document.getElementById('fullP');
+let smInput = document.getElementById('smP');
+let lgInput = document.getElementById('lgP');
+let cedarInput = document.getElementById('cedar');
+let lmpInput = document.getElementById('lastMinP');
 
-let lowSecTot = document.getElementById('lowSecTot')
-let grandTot = document.getElementById('granTot')
+let lowSecTot = document.getElementById('lowSecTot');
+let upperSecTot2 = document.getElementById('upTot2');
+let grandTot = document.getElementById('granTot');
 
-let calc1Button = document.getElementById('calc1')
+let calc1Button = document.getElementById('calc1');
+let calc2Button = document.getElementById('calc2');
 
 calc1Button.addEventListener('click', (e)=>{
     let num1 = compassInput.value;
@@ -33,24 +31,19 @@ calc1Button.addEventListener('click', (e)=>{
     let num4 = flasksInput.value;
     let num5 = tentsInput.value;
     let num6 = smoresInput.value;
+
     let result = Number(num1) + Number(num2) + Number(num3) 
     + Number(num4) + Number(num5) + Number(num6);
     totalScore1.innerHTML = result;
-
     totalScore1.innerHTML >= 63 ? bonusPoints.innerHTML = 35: 
     bonusPoints.innerHTML = 0;
    
     Array.from(upperSecTot).forEach(function(totalsecelement){
         let tot1 = totalScore1.innerHTML
         let tot2 = bonusPoints.innerHTML
-        
         totalsecelement.innerHTML = Number(tot1) + Number(tot2)
     });
-   
 });
-
-let calc2Button = document.getElementById('calc2')
-let upperSecTot2 = document.getElementById('upTot2')
 
 calc2Button.addEventListener('click', (e)=>{
     let num1 = threeKindInput.value;
@@ -66,9 +59,3 @@ calc2Button.addEventListener('click', (e)=>{
     lowSecTot.innerHTML = result;
     grandTot.innerHTML = Number(lowSecTot.innerHTML) + Number(upperSecTot2.innerHTML)
 });
-
-
-
-
-
-//show ending score on page
